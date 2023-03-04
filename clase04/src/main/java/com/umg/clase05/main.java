8e7d11a3cd178fa661a80f6c3cd94a0e83ff2452
ING. JOSE VINICIO PEÑA <jroman1901@hotmail.com>
2023-03-03 23:24:18 -0600

    agregando cambios de listas simples
    funcion de lista simple

---
+++ clase04/src/main/java/com/umg/clase05/main.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.umg.clase05;
+
+/**
+ *
+ * @author DELL
+ */
+public class main {
+    
+    public static void main(String args[])
+    {
+        System.out.println("iniciando");
+        Lista l  = new Lista();
+        l.AgregarInicio(10);
+       l.AgregarInicio(20);
+       l.AgregarInicio(30);
+       l.AgregarInicio(40);
+       l.InsertarFinal(100);
+       
+        l.listar();
+        
+        
+        
+    }    
+}
